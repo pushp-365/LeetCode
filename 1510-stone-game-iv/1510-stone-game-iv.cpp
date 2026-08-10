07ce9a55cdbd1d61a676b554523ed9f1f14a6b23
Pushp Sharma <pushpshar1@gmail.com>
2026-08-11 00:25:39 +0530

    Time: 34 ms (73.12%), Space: 8.9 MB (82.55%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<bool> dp(n + 1, false);
+
+        // dp[i] = true  -> current player can win
+        // dp[i] = false -> current player loses
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                // If we can move to a losing state,
+                // current player wins.
+                if (!dp[i - j * j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
